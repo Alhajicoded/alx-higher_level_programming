1c7fb01a143704181b47ddeb6a17c0de4255991f
Alhajicoded <babxcoded5@gmail.com>
2023-06-14 14:32:23 -0700

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,42 +1,42 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+	  constructor (w, h) {
+		      if (w > 0 && h > 0) {
+			            this.width = w;
+			            this.height = h;
+			          }
+		    }
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
+	  print () {
+		      for (let i = 0; i < this.height; i++) {
+			            console.log('X'.repeat(this.width));
+			          }
+		    }
 
-  rotate () {
-    const a = this.width;
-    this.width = this.height;
-    this.height = a;
-  }
+	  rotate () {
+		      const a = this.width;
+		      this.width = this.height;
+		      this.height = a;
+		    }
 
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
+	  double () {
+		      this.width *= 2;
+		      this.height *= 2;
+		    }
 }
 
 module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
+	  constructor (size) {
+		      super(size, size);
+		    }
 
-  charPrint (c) {
-    if (typeof c === 'undefined') {
-      c = 'X';
-    }
-    for (let i = 0; i < this.width; i++) {
-      console.log(`${c}`.repeat(this.width));
-    }
-  }
+	  charPrint (c) {
+		      if (typeof c === 'undefined') {
+			            c = 'X';
+			          }
+		      for (let i = 0; i < this.width; i++) {
+			            console.log(`${c}`.repeat(this.width));
+			          }
+		    }
 };
